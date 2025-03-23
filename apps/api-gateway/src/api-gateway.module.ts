b0cb3a5679a42f1f2c57e1f3792a145c1d04935f
emaldie <fakeyamuxa@gmail.com>
2025-03-23 20:00:38 +0300

    feat: basic auth module in api-gateway

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
 import { UsersModule } from './users/users.module';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [ApiGatewayController],
-  providers: [ApiGatewayService],
+  imports: [UsersModule, AuthModule],
+  controllers: [ApiGatewayController, AuthController],
+  providers: [ApiGatewayService, AuthService],
 })
 export class ApiGatewayModule {}
