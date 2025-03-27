c3f3b26e1bd4fb62e29a97202559e50030856ca2
emaldie <fakeyamuxa@gmail.com>
2025-03-27 19:20:50 +0300

    feat: prisma module in common library; minor formatting fixes

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -4,23 +4,27 @@ import { ApiGatewayService } from './api-gateway.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi'
+import * as Joi from 'joi';
 
 @Module({
-  imports: [UsersModule, AuthModule, ConfigModule.forRoot({
-    isGlobal: true,
-    validationSchema: Joi.object({
-      DATABASE_URL: Joi.string().required(),
-      POSTGRES_USER: Joi.string().required(),
-      POSTGRES_PASSWORD: Joi.string().required(),
-      POSTGRES_DB: Joi.string().required(),
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+      }),
+      envFilePath: './apps/api-gateway/.env',
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
     }),
-    envFilePath: './apps/api-gateway/.env',
-    validationOptions: {
-      allowUnknown: true,
-      abortEarly: true,
-    },
-  }),],
+  ],
   controllers: [ApiGatewayController],
   providers: [ApiGatewayService],
 })
--- apps/api-gateway/src/api-gateway.service.ts
+++ apps/api-gateway/src/api-gateway.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@blossom/common';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class ApiGatewayService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+export class ApiGatewayService {}
