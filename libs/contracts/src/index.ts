f36838186742945f7494fe7459f5ad45d4517559
emaldie <fakeyamuxa@gmail.com>
2025-03-29 17:07:40 +0300

    fix: add necessary index.ts exports in contracts library

--- libs/contracts/src/index.ts
+++ libs/contracts/src/index.ts
@@ -1,5 +1,8 @@
 export * from './constants/services';
 
 export * from './users/users.patterns';
+export * from './users/dto/create-user.dto'
+export * from './users/dto/update-user.dto'
+export * from './users/dto/user.dto'
 
 export * from './auth/auth.patterns';
